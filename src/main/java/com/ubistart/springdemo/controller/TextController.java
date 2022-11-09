190867e4fe4ff2fd4e826bccd52c017d968bf82a
Fernando <fernando.csconceicao@outlook.com>
2022-11-09 19:52:54 -0300

    Criação de um Dto, que é uma classe que contem as informações de entrada e/ou saída da API e mudança de noma da classe Controller

--- src/main/java/com/ubistart/springdemo/controller/TextController.java
+++ src/main/java/com/ubistart/springdemo/controller/TextController.java
@@ -1,19 +1,23 @@
 package com.ubistart.springdemo.controller;
 
+import com.ubistart.springdemo.dto.TextDto;
+import com.ubistart.springdemo.entity.Text;
 import com.ubistart.springdemo.service.TextService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("ImageController")
 @RequestMapping("/text")
-public class ImageController {
+public class TextController {
     @Autowired
     private TextService textService;
 
     @RequestMapping("/save")
-    public String saveText(){
-       return textService.save("Test");
+    public String saveText(@RequestBody TextDto dto){
+        Text text = textService.save(dto);
+       return text.getText();
     }
 
 }
---
+++ src/main/java/com/ubistart/springdemo/dto/TextDto.java
@@ -0,0 +1,8 @@
+package com.ubistart.springdemo.dto;
+
+import lombok.Data;
+
+@Data
+public class TextDto {
+    private String text;
+}
