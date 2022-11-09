362d4f6161df617f606c1e06dd0132af92859560
Fernando <fernando.csconceicao@outlook.com>
2022-11-09 19:54:26 -0300

    Mudanças necessárias para rodar. Já que a classe a ser guardada não pode ser uma String diretamente. E sim, uma classe anotada com @Entity que continha uma String

--- src/main/java/com/ubistart/springdemo/repository/TextRepository.java
+++ src/main/java/com/ubistart/springdemo/repository/TextRepository.java
@@ -1,8 +1,9 @@
 package com.ubistart.springdemo.repository;
 
+import com.ubistart.springdemo.entity.Text;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TextRepository extends JpaRepository<String, Long> {
+public interface TextRepository extends JpaRepository<Text, Long> {
 }
--- src/main/java/com/ubistart/springdemo/service/TextService.java
+++ src/main/java/com/ubistart/springdemo/service/TextService.java
@@ -1,5 +1,7 @@
 package com.ubistart.springdemo.service;
 
+import com.ubistart.springdemo.dto.TextDto;
+import com.ubistart.springdemo.entity.Text;
 import com.ubistart.springdemo.repository.TextRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,10 +11,11 @@ public class TextService {
     @Autowired
     private TextRepository textRepository;
 
-    public String save(String text) {
-        if(text.contains("*")){
+    public Text save(TextDto dto) {
+        if(dto.getText().contains("*")){
             return null;
         }
+        Text text =  new Text(null, dto.getText());
         return textRepository.save(text);
     }
 }
